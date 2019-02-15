cfc76e643553518687ea921854064500137c28df
Juraj Komacka <juraj.komacka@firma.seznam.cz>
2019-02-15 20:19:45 +0100

    Add credit column to model User

--- iou/models.py
+++ iou/models.py
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable=False)
+    credit = db.Column(db.Integer, default=0)
 
     def __repr__(self):
         return '<User %r>' % self.email
--- tests/test_login.py
+++ tests/test_login.py
@@ -15,5 +15,11 @@ class LoginTest(TestBase):
             info = self.call('/user')
             self.assertEqual(email, info['user']['email'])
 
+     def test_new_user_has_zero_credit(self):
+        with self.client:
+            self.login('credit_zero@test.com')
+            info = self.call('/user')
+            self.assertEqual(0, info['user']['credit'])
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
